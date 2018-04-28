dd8442ee283e68856215ed5bd5b8a94bf37fd0d1
Gabriel Falcão <gabriel@nacaolivre.org>
2018-04-28 02:56:17 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-import ast
 import os
 from setuptools import setup, find_packages
 
@@ -11,28 +9,16 @@ local_file = lambda *f: \
     open(os.path.join(os.path.dirname(__file__), *f)).read()
 
 
-class VersionFinder(ast.NodeVisitor):
-    VARIABLE_NAME = 'version'
-
-    def __init__(self):
-        self.version = None
-
-    def visit_Assign(self, node):
-        try:
-            if node.targets[0].id == self.VARIABLE_NAME:
-                self.version = node.value.s
-        except:
-            pass
-
-
 def read_version():
-    finder = VersionFinder()
-    finder.visit(ast.parse(local_file('agentzero', 'version.py')))
-    return finder.version
+    ctx = {}
+    exec(local_file('agentzero', 'version.py'), ctx)
+    return ctx['version']
 
 
 dependencies = [
     'pyzmq==16.0.2',
+    'msgpack==0.5.6',
+    'six==1.11.0',
 ]
 
 
@@ -43,6 +29,7 @@ setup(
         'AgentZero lets you create, connect, bind, and modify zeromq sockets in runtime with ease.',
         'It works great with gevent, making it possible to create network applications with simple code that performs complex operations.',
     ]),
+    long_description=local_file('README.rst'),
     entry_points={
         'console_scripts': ['agentzero = agentzero.console.main:entrypoint'],
     },
